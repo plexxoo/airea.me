9e441d1a33993a8d23fa8025e36e32da95c35f43
Jesús Martínez <jamarcer@caronte.(none)>
2011-05-07 00:48:23 +0200

    Definición de instancia de trabajo.
    Eliminada la opción da auth (sistema de acceso a la aplicación), añadidas definiciones de elementos que
    facilitan el posterior uso de la aplicación en a0.py.
    
    Incluido modelo de cabecera en fichero controllers/default.py
    
    Committer: Jesús Martínez <jamarcer (jamarcer@plexxoo.com)>

---
+++ aireame/models/a0.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*- 
+# Este fichero forma parte de airea.me
+#
+# Copyright 2011 Plexxoo Interactiva S.L.
+# Copyright Daniel Gonzalez     <demetrio@plexxoo.com>
+# Copyright Jon Latorre         <moebius@plexxoo.com>
+# Copyright Silvia Martín       <smartin@plexxoo.com>
+# Copyright Jesus Martinez      <jamarcer@plexxoo.com>
+#
+# Este fichero se distribuye bajo la licencia GPL según las
+# condiciones que figuran en el fichero 'licence' que se acompaña.
+# Si se distribuyera este fichero individualmente, DEBE incluirse aquí
+# las condiciones expresadas allí.
+#
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+import os
+
+# Base path
+w2p_path=request.env.web2py_path
+
+# Basic PATH
+W2P_APP_PATH=os.path.join(w2p_path,'applications')
+APP_PATH=os.path.join(W2P_APP_PATH,request.application)
+
+# DAL definition
+DB_DAL='sqlite://storage.db'
