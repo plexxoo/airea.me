646101629f210814d4a71b3f221292d8dba32d61
Jesús Martínez <jamarcer@plexxoo.com>
2011-05-07 01:34:38 +0200

    Provincias.
    Añadido el controlador, modelo y vistas para la gestión de provincias.
    
    Modificado el menu para añadir este maestro.
    
    Subidos cambios en db.py

--- aireame/models/db.py
+++ aireame/models/db.py
@@ -1,78 +1,52 @@
 # -*- coding: utf-8 -*-
-# this file is released under public domain and you can use without limitations
+# Este fichero forma parte de airea.me
+#
+# Copyright 2011 Plexxoo Interactiva S.L.
+# Copyright Daniel Gonzalez     <demetrio@plexxoo.com>
+# Copyright Jon Latorre         <moebius@plexxoo.com>
+# Copyright Silvia Martín       <smartin@plexxoo.com>
+# Copyright Jesus Martinez      <jamarcer@plexxoo.com>
+#
+# Este fichero se distribuye bajo la licencia GPL según las
+# condiciones que figuran en el fichero 'licence' que se acompaña.
+# Si se distribuyera este fichero individualmente, DEBE incluirse aquí
+# las condiciones expresadas allí.
+#
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-#########################################################################
-## This scaffolding model makes your app work on Google App Engine too
-#########################################################################
-
-if request.env.web2py_runtime_gae:            # if running on Google App Engine
-    db = DAL('google:datastore')              # connect to Google BigTable
-                                              # optional DAL('gae://namespace')
-    session.connect(request, response, db = db) # and store sessions and tickets there
-    ### or use the following lines to store sessions in Memcache
-    # from gluon.contrib.memdb import MEMDB
-    # from google.appengine.api.memcache import Client
-    # session.connect(request, response, db = MEMDB(Client()))
-else:                                         # else use a normal relational database
-    db = DAL('sqlite://storage.sqlite')       # if not, use SQLite or other DB
-## if no need for session
-# session.forget()
-
-#########################################################################
-## Here is sample code if you need for
-## - email capabilities
-## - authentication (registration, login, logout, ... )
-## - authorization (role based authorization)
-## - services (xml, csv, json, xmlrpc, jsonrpc, amf, rss)
-## - crud actions
-## (more options discussed in gluon/tools.py)
-#########################################################################
-
+db = DAL(DB_DAL)    
 from gluon.tools import *
-mail = Mail()                                  # mailer
+#mail = Mail()                                  # mailer
 auth = Auth(globals(),db)                      # authentication/authorization
 crud = Crud(globals(),db)                      # for CRUD helpers using auth
 service = Service(globals())                   # for json, xml, jsonrpc, xmlrpc, amfrpc
 plugins = PluginManager()
 
-mail.settings.server = 'logging' or 'smtp.gmail.com:587'  # your SMTP server
-mail.settings.sender = 'you@gmail.com'         # your email
-mail.settings.login = 'username:password'      # your credentials or None
+#mail.settings.server = 'logging' or 'smtp.gmail.com:587'  # your SMTP server
+#mail.settings.sender = 'you@gmail.com'         # your email
+#mail.settings.login = 'username:password'      # your credentials or None
 
 auth.settings.hmac_key = 'sha512:2a6d8cd8-0cbc-49e6-850f-3b6c39fab688'   # before define_tables()
 auth.define_tables()                           # creates all needed tables
-auth.settings.mailer = mail                    # for user email verification
+#auth.settings.mailer = mail                    # for user email verification
 auth.settings.registration_requires_verification = False
 auth.settings.registration_requires_approval = False
 auth.messages.verify_email = 'Click on the link http://'+request.env.http_host+URL('default','user',args=['verify_email'])+'/%(key)s to verify your email'
 auth.settings.reset_password_requires_verification = True
 auth.messages.reset_password = 'Click on the link http://'+request.env.http_host+URL('default','user',args=['reset_password'])+'/%(key)s to reset your password'
-
-#########################################################################
-## If you need to use OpenID, Facebook, MySpace, Twitter, Linkedin, etc.
-## register with janrain.com, uncomment and customize following
-# from gluon.contrib.login_methods.rpx_account import RPXAccount
-# auth.settings.actions_disabled=['register','change_password','request_reset_password']
-# auth.settings.login_form = RPXAccount(request, api_key='...',domain='...',
-#    url = "http://localhost:8000/%s/default/user/login" % request.application)
-## other login methods are in gluon/contrib/login_methods
-#########################################################################
+auth.settings.actions_disabled = ['profile','change_password','retrieve_username','request_reset_password']
 
 crud.settings.auth = None                      # =auth to enforce authorization on crud
-
-#########################################################################
-## Define your tables below (or better in another model file) for example
-##
-## >>> db.define_table('mytable',Field('myfield','string'))
-##
-## Fields can be 'string','text','password','integer','double','boolean'
-##       'date','time','datetime','blob','upload', 'reference TABLENAME'
-## There is an implicit 'id integer autoincrement' field
-## Consult manual for more options, validators, etc.
-##
-## More API examples for controllers:
-##
-## >>> db.mytable.insert(myfield='value')
-## >>> rows=db(db.mytable.myfield=='value').select(db.mytable.ALL)
-## >>> for row in rows: print row.id, row.myfield
-#########################################################################
