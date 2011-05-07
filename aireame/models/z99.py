c6b924e4abf3256397bf0fcff1aeeb620c8eeaa7
Jesús Martínez <jamarcer@plexxoo.com>
2011-05-07 13:39:44 +0200

    Modificado modelo que automatiza todos los controladores la vista movil.
    Metido control para evitar que funcione en caso de ir desde modo script.

--- aireame/models/z99.py
+++ aireame/models/z99.py
@@ -1,14 +1,13 @@
-import os
-detect = local_import("detect")
+if request.env.http_user_agent:
+    import os
+    detect = local_import("detect")
+        
     
-
-is_mobile = detect.detect_mobile_browser(request.env.http_user_agent)
-#forzar vista movil:
-#is_mobile = True
-if is_mobile:
-    #Es un movil             
-    response.view = "%s/%s.%s" % (request.controller, request.function, "mobile")
-    if not os.path.exists(os.path.join(request.folder, 'views', request.controller, request.function+".mobile")):
-        response.view = '%s.%s' % ("generic", "mobile") 
-
-
+    is_mobile = detect.detect_mobile_browser(request.env.http_user_agent)
+    #forzar vista movil:
+    #is_mobile = True
+    if is_mobile:
+        #Es un movil             
+        response.view = "%s/%s.%s" % (request.controller, request.function, "mobile")
+        if not os.path.exists(os.path.join(request.folder, 'views', request.controller, request.function+".mobile")):
+            response.view = '%s.%s' % ("generic", "mobile") 
