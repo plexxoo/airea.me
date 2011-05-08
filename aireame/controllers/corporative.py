ffcc575872a52ad14c503f0a48e615cacfa3e792
Daniel Gonzalez <demetrio@plexxoo.com>
2011-05-08 15:02:38 +0200

    Añadido controlador para paginas estaticas "corporative.py"

---
+++ aireame/controllers/corporative.py
@@ -0,0 +1,17 @@
+# coding: utf8
+# try something like
+#def index(): return dict()
+
+def about_us(): return dict()
+
+def contact(): return dict()
+
+def field(): return dict()
+
+def license(): return dict()
+
+def press(): return dict()
+
+def privacity(): return dict()
+
+def terms_of_use(): return dict()
